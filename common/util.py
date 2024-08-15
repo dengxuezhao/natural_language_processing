7a427e0cd0c209492eec6732bf8e430e74e76f8a
xuezhao's 1st pc <xuezhao.d@gmail.com>
2024-08-15 08:33:11 +0800

    增加preproess

--- common/util.py
+++ common/util.py
@@ -1,5 +1,24 @@
 import re
+import numpy as np 
 
 def preprocess(text):
     text = text.lower()
-    text = text.replace
+    words = re.findall(r'\w+|[^\w\s]', text)  # 使用单词字符进行匹配
+    
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+    
+    corpus = np.array([word_to_id[w] for w in words])
+    return corpus, word_to_id, id_to_word
+
+if __name__ == '__main__':
+    text = 'You say goodbye and I say hello.'
+    corpus, word_to_id, id_to_word = preprocess(text)
+    print(corpus)
+    print(word_to_id)
+    print(id_to_word)
